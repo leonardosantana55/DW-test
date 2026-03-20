282f1d5fe3e955a3d0be3ee012f4dfa516a2a409
SANTANA Leonardo (ENGIE BRASIL ENERGIA S.A.) <xq6460@engie.com>
2026-03-20 20:29:12 +0100

    table from csv works

--- ddl.py
+++ ddl.py
@@ -7,7 +7,7 @@ dbname = "mydb"
 user = "XQ6460"
 password = "08021994"
 
-#connect to pg
+# connect to pg
 conn = psycopg2.connect(
     host=host,
     dbname=dbname,
@@ -16,10 +16,12 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-#read csv files on a folder
-#TODO test for subdirectories
+# read csv files on a path
+# TODO test for subdirectories
 path = "./data"
 files = glob.glob(path + '/**/*.csv', recursive=True)
+
+# create a table for each file in the directory
 for f in files:
     table_name = f.removeprefix("./data/").removesuffix(".csv")
     with open(f, encoding="utf8") as f:
@@ -27,14 +29,23 @@ for f in files:
         header = next(reader)
         sample = [next(reader) for _ in range(5)]
 
+    # create a list of sql.SQL objects with the column name and type
     cols = []
     sql_query = ""
     for col_name in header:
+        # fix some problems with utf8 csv reading and bad naming
         safe_name = col_name.replace(" ", "_")
-
-    cols.append(f"{sql.Identifier(safe_name)} TEXT")
-
-    sql_query = sql.SQL("CREATE TABLE IF NOT EXISTS {table} ({cols})")\
-    .format(table=sql.Identifier(table_name), cols=sql.SQL(', ').join(cols))
-
-# TODO fiquei impacado nessa parte de compor a string. tenho que fumar um tabaco e ler a documentação com calma. https://www.psycopg.org/docs/sql.html#psycopg2.sql.Identifier
+        safe_name = col_name.replace("\ufeff", "") # fix for BOM
+        # append to the cols list in a format that psycopg2 wants
+        cols.append(
+            sql.SQL("{} {}").format(
+                sql.Identifier(safe_name),
+                sql.SQL("VARCHAR(255)")))
+    # compose the final query
+    sql_query = sql.SQL("CREATE TABLE IF NOT EXISTS {table} ({cols})").format(
+        table=sql.Identifier(table_name),
+        cols=sql.SQL(", ").join(cols))
+    # execute query
+    cur.execute(sql_query)
+    # make the changes to the database persistent
+    conn.commit()
